93ad2e292e2c6655073eb48ac0558e6f2c15984b
robinrawat30 <robinrawat4201@gmail.com>
2025-01-29 00:25:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+require("dotenv").config();
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.get('/twiter', (req, res) => {
+    res.send('Hello twiter!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
